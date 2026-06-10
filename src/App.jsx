3279e2d3cd48ea5615bda5ff522a8576e58c25d3
Babs-Hakeem <babshakeem786@gmail.com>
2026-06-10 18:07:36 +0100

    feat: add complete student side

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,31 @@
 import { Routes, Route } from "react-router-dom";
+
+// Admin (her side - don't touch)
 import AdminLayout from "./layout/AdminLayout";
 import Dashboard from "./pages/admin/Dashboard";
+
+// Student (your side)
+import StudentLayout from "./components/student/StudentLayout";
+import StudentDashboard from "./pages/student/StudentDashboard";
+import MyCourses from "./pages/student/MyCourses";
+import LessonPage from "./pages/student/LessonPage";
 
 function App() {
   return (
     <Routes>
+      {/* Admin routes */}
       <Route path="/" element={<AdminLayout />}>
         <Route index element={<Dashboard />} />
+      </Route>
+
+      {/* Student routes */}
+      <Route path="/student" element={<StudentLayout />}>
+        <Route path="dashboard" element={<StudentDashboard />} />
+        <Route path="courses" element={<MyCourses />} />
+        <Route
+          path="courses/:courseId/lessons/:lessonId"
+          element={<LessonPage />}
+        />
       </Route>
     </Routes>
   );
